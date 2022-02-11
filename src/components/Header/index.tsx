b238c9aeeceb59a72f6c93580594f9feef19dcd9
Matheus Cruz <matheuswachcruz@gmail.com>
2022-02-10 21:05:29 -0300

    feat: create plants environments list

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper"
+
+import avatarProfile from "../../assets/avatar_profile.png"
 
 import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
 
 export function Header() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Hi,</Text>
+        <Text style={styles.userName}>Matheus</Text>
+      </View>
 
+      <Image style={styles.avatarImage} source={avatarProfile} />
     </View>
   )
 }
@@ -18,6 +27,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     paddingVertical: 20,
-    backgroundColor: colors.red,
+    // the getStatusBarHeight() method return the status bar height and we apply this value as margin top
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+  },
+  avatarImage: {
+    width: 75,
+    height: 75,
+    borderRadius: 40,
   }
 })
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
+import { EnvironmentButton } from '../components/EnvironmentButton';
 import { Header } from '../components/Header';
 
+import { api } from "../services/api"
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
 
 export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+
+  useEffect(() => {
+    const getPlants = async () => {
+      const { data } = await api.get('plants_environments');
+
+      setEnvironments([{
+        key: 'all',
+        title: 'All'
+      }, ...data])
+    }
+
+    getPlants();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
       <Header />
+
+      <Text style={styles.title}>In which environment</Text>
+      <Text style={styles.subtitle}>do you want to place your plant?</Text>
+      </View>
+
+    <View>
+      <FlatList data={environments} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.enviromentList} renderItem={({ item }) => (<EnvironmentButton key={item?.key} title={item?.title} />)} />
+    </View>
+
     </View>
   );
 }
@@ -16,8 +50,29 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // backgroundColor: colors.background,
+    backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {  
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.text,
+    lineHeight: 20,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   }
 })
